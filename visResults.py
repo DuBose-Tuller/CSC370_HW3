2abaf59ebc1161b0e4b2699f71235b8b33e97f00
DuBose Tuller <d.tuller@wustl.edu>
2023-04-11 21:46:33 -0400

    make results cleaner

--- visResults.py
+++ visResults.py
@@ -14,12 +14,12 @@ with open("results.txt", "r+") as f:
         p2wins = f.readline()
 
         result = int(str(p1wins).split(" ")[1]) - int(str(p2wins).split(" ")[1])
-        if result > 0:
-            M[i % NUM_BOTS][i//NUM_BOTS] = "W"
-        elif result < 0:
-            M[i % NUM_BOTS][i//NUM_BOTS] = "L"
+        if result > 200:
+            M[i//NUM_BOTS][i % NUM_BOTS] = "W"
+        elif result < -200:
+            M[i//NUM_BOTS][i % NUM_BOTS] = "L"
         else:
-            M[i % NUM_BOTS][i//NUM_BOTS] = "D"
+            M[i//NUM_BOTS][i % NUM_BOTS] = "D"
 
 df = pd.DataFrame(M)
 df.to_csv("table.csv")
